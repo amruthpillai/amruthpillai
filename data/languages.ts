78a918f1ec45cd9ae77f0d616836f7bf17101cc0
owalishawon <owali.shawon@gmail.com>
2025-04-20 16:44:17 +0600

    Update languages.ts

--- data/languages.ts
+++ data/languages.ts
@@ -8,33 +8,9 @@ const languagesList: Language[] = [
   },
   {
     id: 2,
-    language: "Tamil",
-    text: "தமிழ் என் தாய்மொழி.",
+    language: "Bengali",
+    text: "আমি বাংলা ভাষায় কথা বলি।",
     translation: "Tamil is my native tongue.",
-  },
-  {
-    id: 3,
-    language: "Kannada",
-    text: "ನನಗೆ ಕನ್ನಡ ಕೂಡ ಸ್ವಲ್ಪ ಬರುತ್ತೆ.",
-    translation: "I know a bit of Kannada as well.",
-  },
-  {
-    id: 4,
-    language: "Hindi",
-    text: "में मुश्किल से हिंदी बात करता हूँ|",
-    translation: "I speak Hindi with some difficulty.",
-  },
-  {
-    id: 5,
-    language: "German",
-    text: "Ich lerne Deutsch auf Duolingo.",
-    translation: "I am currently learning German on Duolingo.",
-  },
-  {
-    id: 6,
-    language: "Italian",
-    text: "Bella Ciao, Bella Ciao, Bella Ciao Ciao Ciao!",
-    translation: "That's all the Italian I know. Credits: Money Heist",
   },
 ];
 
