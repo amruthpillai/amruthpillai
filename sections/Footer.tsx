83b6f7e202306bef98260e1f9e10dda02a55777d
owalishawon <owali.shawon@gmail.com>
2025-04-20 17:02:13 +0600

    Update Footer.tsx

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 const Footer = () => (
   <div id="footer" className="mb-16 text-xs leading-loose opacity-30">
-    Licensed under MIT.
+    Inspired from <a href="https://github.com/AmruthPillai/ResumeOnTheWeb">ResumeOnTheWeb</a>
     <br />
     Copyright {format(Date.now(), "yyyy")} Owali Ullah Shawon.
   </div>
