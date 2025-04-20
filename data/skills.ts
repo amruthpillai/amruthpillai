8be5e76983e7bafb90da1ddbd9128c355d441079
owalishawon <owali.shawon@gmail.com>
2025-04-20 16:39:27 +0600

    Update skills.ts

--- data/skills.ts
+++ data/skills.ts
@@ -11,7 +11,7 @@ const skills: Skill[] = [
     id: 2,
     icon: "/images/skills/angular.png",
     name: "Frontend Javascript Frameworks",
-    technologies: ["Angular", "React", "Vue.js", "Svelte"],
+    technologies: ["React", "Vue.js"],
   },
   {
     id: 3,
@@ -23,55 +23,25 @@ const skills: Skill[] = [
     id: 4,
     icon: "/images/skills/flutter.png",
     name: "Cross Platform App Development",
-    technologies: ["Dart", "Flutter", "Firebase"],
-  },
-  {
-    id: 5,
-    icon: "/images/skills/python.png",
-    name: "Scripting Frameworks",
-    technologies: ["Python", "Rust", "Selenium", "Puppeteer"],
+    technologies: ["React Native", "electron", "Firebase"],
   },
   {
     id: 6,
     icon: "/images/skills/database.png",
     name: "Database Management System",
-    technologies: ["Postgres", "TypeORM", "MongoDB", "Mongoose"],
+    technologies: ["MySql", "Postgres", "Prisma", "TypeORM", "MongoDB", "Mongoose"],
   },
   {
     id: 7,
-    icon: "/images/skills/android.png",
-    name: "Android App Development",
-    technologies: ["Android Studio", "Java", "Kotlin", "XML"],
-  },
-  {
-    id: 8,
-    icon: "/images/skills/sketch.png",
-    name: "UI/UX Design",
-    technologies: ["Adobe XD", "Figma", "Sketch"],
-  },
-  {
-    id: 9,
-    icon: "/images/skills/tensorflow.png",
-    name: "Data Processing & Machine Learning",
-    technologies: ["R", "scikit-learn", "TensorFlow", "Keras"],
-  },
-  {
-    id: 10,
-    icon: "/images/skills/seo.png",
-    name: "Search Engine Optimization",
-    technologies: ["PageRank", "Backlinks", "Google Analytics"],
-  },
-  {
-    id: 11,
     icon: "/images/skills/photography.png",
     name: "Photography & Cinematography",
     technologies: ["Adobe Photoshop", "Adobe Lightroom"],
   },
   {
-    id: 12,
+    id: 8,
     icon: "/images/skills/fcpx.png",
     name: "Photo & Video Post-Processing",
-    technologies: ["Adobe After Effects", "Final Cut Pro X"],
+    technologies: ["Cnva", "Final Cut"],
   },
 ];
 
